bb324d2b50b637066a6ef391f633f0c7029100de
Adan Ordoñez <joseadan_16@hotmail.com>
2024-09-02 20:10:15 -0600

    Carga Inicial de archivos del proyecto

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,14 @@
+
+export const metadata = {
+    title: "SEO Contact",
+    description:"SEO Contact Page"
+}
+
+export default function ContactPage(){
+    return(
+        <>
+            <span className="text-7xl">Contact Page</span>
+        </>
+    );
+
+}
---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,13 @@
+
+export const metadata = {
+    title:"SEO Title",
+    description: "SEO Description"
+}
+
+export default function PricingPage(){
+    return(
+        <>
+            <span className="text-7xl" >Princing Page</span>
+        </>
+    );
+}
